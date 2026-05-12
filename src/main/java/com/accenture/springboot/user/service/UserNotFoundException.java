845e7ed0957d700183768741ceb5a0119404a2a8
sbarrasa <zaiper2000@gmail.com>
2026-05-12 18:00:29 -0300

    Optimización de Exceptions

--- src/main/java/com/accenture/springboot/user/service/UserNotFoundException.java
+++ src/main/java/com/accenture/springboot/user/service/UserNotFoundException.java
@@ -3,6 +3,6 @@ package com.accenture.springboot.user.service;
 public class UserNotFoundException extends RuntimeException {
 
     public UserNotFoundException(Integer id) {
-        super("User not found with id: " + id);
+        super("User %s not found".formatted(id));
     }
 }

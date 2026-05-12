2a4f67481c80249bf38b53b0f39ffe04a2196c43
sergio.g.rozenberg <sergio.g.rozenberg@accenture.com>
2026-05-12 13:04:57 -0300

    mejoras en exception

--- src/main/java/com/accenture/springboot/exception/UserAlreadyExistsException.java
+++ src/main/java/com/accenture/springboot/exception/UserAlreadyExistsException.java
@@ -1,8 +1,11 @@
 package com.accenture.springboot.exception;
 
-public class UserAlreadyExistsException extends RuntimeException {
+public class UserException extends RuntimeException {
 
     public UserAlreadyExistsException(Integer id) {
         super("User already exists with id: " + id);
+    }
+    public UserNotFoundException(Integer id) {
+        super("User not found with id: " + id);
     }
 }

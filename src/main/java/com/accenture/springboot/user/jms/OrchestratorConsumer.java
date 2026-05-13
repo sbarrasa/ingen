71306c532be86daa7e6f2a9978ee98855c9f886d
Ghibaudo <alberto.ghibaudo@accenture.com>
2026-05-13 14:49:08 -0300

    Agregar jms

---
+++ src/main/java/com/accenture/springboot/user/jms/OrchestratorConsumer.java
@@ -0,0 +1,19 @@
+package com.accenture.springboot.user.jms;
+
+import com.accenture.springboot.user.api.UserEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrchestratorConsumer {
+
+    private static final Logger logger = LoggerFactory.getLogger(OrchestratorConsumer.class);
+
+    @JmsListener(destination = "${user.updates.queue}")
+    public void consumeUserEvent(UserEvent event) {
+        logger.info("ORCHESTRATOR RECEIVED: Action={}, User={}", event.action(), event.user());
+        // In a real scenario, this would trigger other microservices or workflows
+    }
+}

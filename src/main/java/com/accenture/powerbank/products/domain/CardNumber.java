c26d2bdc3864ff00334bee97cdd9f50bedc35ae9
lisandroaccenture <lisandro.medrano@accenture.com>
2026-05-14 14:03:47 -0300

    Agregamos tests unitarios para Card Number

--- src/main/java/com/accenture/powerbank/products/domain/CardNumber.java
+++ src/main/java/com/accenture/powerbank/products/domain/CardNumber.java
@@ -7,6 +7,7 @@ public record CardNumber(String value) {
         validateNotNull();
         validateNotEmpty();
         validateDigits();
+        validateLength();
     }
 
     private void validateDigits() {
